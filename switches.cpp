dab5ea95893da4e456679f38ba2a576502dad9df
Enoch2k2 <enoch2k2@hotmail.com>
2017-06-23 13:53:42 -0500

    created a switch to check if there was a teenager

--- switches.cpp
+++ switches.cpp
@@ -2,5 +2,32 @@
 using namespace std;
 
 int main(){
+  int age = 20;
+  switch(age){
+    case 13:
+      cout << "You are a teenager!" << endl;
+      break;
+    case 14:
+      cout << "You are a teenager!" << endl;
+      break;
+    case 15:
+      cout << "You are a teenager!" << endl;
+      break;
+    case 16:
+      cout << "You are a teenager!" << endl;
+      break;
+    case 17:
+      cout << "You are a teenager!" << endl;
+      break;
+    case 18:
+      cout << "You are a teenager!" << endl;
+      break;
+    case 19:
+      cout << "You are a teenager!" << endl;
+      break;
+    default:
+      cout << "You are not a teenager!" << endl;
+      break;
+  }
   return 0;
 }
